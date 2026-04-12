abbdb1b5ac4587e7e7b64b979c74aa848c5e18a8
tsumitakaoru <k.tsumita01@gmail.com>
2026-04-12 18:19:47 -0400

    feat: integrate Sentry for error tracking and add configuration to environment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
+import * as Sentry from "@sentry/react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <App />
